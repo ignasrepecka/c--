c816034444d155c6a84fb4449a2479e11e2930ed
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2023-12-29 14:50:01 +0200

    Update generuoti.cpp

--- generuoti.cpp
+++ generuoti.cpp
@@ -6,25 +6,10 @@
 #include <iomanip>
 #include <stdexcept>
 #include "calculateStatistics.h" // Include this
+#include "printResults.h"
 #include "generuoti.h"
 
 using namespace std;
-
-void printResults(vector<Studentas>& studentai, int a) {
-    for(Studentas& studentas : studentai) {
-        if (a == 1) {
-            cout << setw(15) << left << studentas.vardas
-                 << setw(15) << left << studentas.pavarde
-                 << setw(15) << left << fixed << setprecision(2) << studentas.vidurkis << endl;
-        } else if (a == 2) {
-            cout << setw(15) << left << studentas.vardas
-                 << setw(15)<< left << studentas.pavarde
-                 << setw(15) << left << studentas.mediana << endl;
-        } else {
-            throw runtime_error("Neteisingas pasirinkimas a");
-        }
-    }
-}
 
 void generuoti(int n, int a) {
     vector<Studentas> studentai(n);
@@ -54,4 +39,3 @@ void generuoti(int n, int a) {
     // Then, call the function to sort and print the results
     printResults(studentai, a);
 }
-
