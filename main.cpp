9ceae2ed2e8934e56da0b6af97a659d9914cbedc
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2023-12-29 21:20:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <stdexcept>
+#include <limits>
+#include "studentai.h"
+#include "sortStudents.h"
+#include "failas.h"
+#include "calculateStatistics.h"
+#include "printResults.h"
+
+using namespace std;
+
 int main() {
     srand(time(0));
     int n;
@@ -5,18 +18,22 @@ int main() {
     cin >> n;
 
     int b;
-    cout << "Ką pasirinksite?: 1 - Įvesti ranka, 2 - Generuoti atsitiktinai";
+    cout << "Ką pasirinksite?: 1 - Įvesti ranka, 2 - Generuoti atsitiktinai, 3 - Skaityti is failo: ";
     cin >> b;
 
     int a;
     cout << "Ką pasirinksite?: 1 - Vidurkis, 2 - Mediana: ";
     cin >> a;
 
+    vector<Studentas> studentai;
+
     try {
         if (b == 1) {
             ivestiStudentus(n, a);
         } else if (b == 2) {
             generuoti(n, a);
+        } else if (b == 3) {
+            isFailo(studentai, a);
         } else {
             throw runtime_error("Neteisingas pasirinkimas");
         }
