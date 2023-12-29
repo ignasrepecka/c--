209978d63d5d198b4892ad76824c6e711469ab4f
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2023-12-29 23:33:37 +0200

    Update printResults.cpp

--- printResults.cpp
+++ printResults.cpp
@@ -9,12 +9,29 @@
 
 using namespace std;
 
+bool compareByMedian(const Studentas& a, const Studentas& b) {
+    return a.mediana < b.mediana;
+}
+
+bool compareByAverage(const Studentas& a, const Studentas& b) {
+    return a.vidurkis < b.vidurkis;
+}
+
 void printResults(vector<Studentas>& studentai, int a) {
+    // Sort the students by average or median
+    if (a == 1) {
+        sort(studentai.begin(), studentai.end(), compareByAverage);
+    } else if (a == 2) {
+        sort(studentai.begin(), studentai.end(), compareByMedian);
+    }
+
+    // Print the header
     cout << setw(15) << left << "Vardas"
          << setw(15) << left << "Pavade"
          << setw(15) << left << "Vid/Med:"
          << endl;
     cout << "------------------------------------------------------------------" << endl;
+
     for(Studentas& studentas : studentai) {
         if (a == 1) {
             cout << setw(15) << left << studentas.vardas
