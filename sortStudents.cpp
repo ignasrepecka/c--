fa59a98c17e6056e8142933ed5ba546ffb6fcaee
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2023-12-30 18:28:46 +0200

    Update sortStudents.cpp

--- sortStudents.cpp
+++ sortStudents.cpp
@@ -1,5 +1,6 @@
 #include "sortStudents.h"
 #include <algorithm>
+#include <iostream>
 
 bool compareByName(const Studentas& a, const Studentas& b) {
     return a.vardas < b.vardas;
@@ -9,10 +10,34 @@ bool compareBySurname(const Studentas& a, const Studentas& b) {
     return a.pavarde < b.pavarde;
 }
 
-void sortStudents(std::vector<Studentas>& studentai, bool sortByName) {
-    if (sortByName) {
-        std::sort(studentai.begin(), studentai.end(), compareByName);  // To sort by name
-    } else {
-        std::sort(studentai.begin(), studentai.end(), compareBySurname);  // To sort by surname
+bool compareByAverage(const Studentas& a, const Studentas& b) {
+    return a.vidurkis < b.vidurkis;
+}
+
+bool compareByMedian(const Studentas& a, const Studentas& b) {
+    return a.mediana < b.mediana;
+}
+
+void sortStudents(std::vector<Studentas>& studentai) {
+    int sortOption;
+    std::cout << "Kaip norite rūšiuoti studentus?: 1 - Pagal vardą, 2 - Pagal pavardę, 3 - Pagal vidurkį, 4 - Pagal medianą: ";
+    std::cin >> sortOption;
+
+    switch (sortOption) {
+        case 1:
+            std::sort(studentai.begin(), studentai.end(), compareByName);  // To sort by name
+            break;
+        case 2:
+            std::sort(studentai.begin(), studentai.end(), compareBySurname);  // To sort by surname
+            break;
+        case 3:
+            std::sort(studentai.begin(), studentai.end(), compareByAverage);  // To sort by average
+            break;
+        case 4:
+            std::sort(studentai.begin(), studentai.end(), compareByMedian);  // To sort by median
+            break;
+        default:
+            std::cout << "Netinkamas rūšiavimo pasirinkimas. Prašome pasirinkti nuo 1 iki 4." << std::endl;
+            break;
     }
 }
