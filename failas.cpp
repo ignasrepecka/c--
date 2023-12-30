f98af1a7850e3a8765989fe3d5bad0336aebc424
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2023-12-30 14:55:35 +0200

    Update failas.cpp

--- failas.cpp
+++ failas.cpp
@@ -55,14 +55,20 @@ void isFailo(std::vector<Studentas>& studentai, int a, const string& filename) {
     ofstream failas1("kietiakai" + to_string(studentai.size()) + ".txt");
     ofstream failas2("vargsiukai" + to_string(studentai.size()) + ".txt");
     for (const Studentas& studentas : studentai) {
-        if (studentas.vidurkis >= 5) {
-            failas1 << studentas.vardas << "\t" << studentas.pavarde << "\t" << studentas.vidurkis << endl;
-        } else {
-            failas2 << studentas.vardas << "\t" << studentas.pavarde << "\t" << studentas.vidurkis << endl;
+        if (a == 1) {
+            if (studentas.vidurkis >= 5) {
+                failas1 << studentas.vardas << "\t" << studentas.pavarde << "\t" << studentas.vidurkis << endl;
+            } else {
+                failas2 << studentas.vardas << "\t" << studentas.pavarde << "\t" << studentas.vidurkis << endl;
+            }
+        } else if (a == 2) {
+            if (studentas.mediana >= 5) {
+                failas1 << studentas.vardas << "\t" << studentas.pavarde << "\t" << studentas.mediana << endl;
+            } else {
+                failas2 << studentas.vardas << "\t" << studentas.pavarde << "\t" << studentas.mediana << endl;
+            }
         }
     }
     failas1.close();
     failas2.close();
-
-    printResults(studentai, a);
 }
