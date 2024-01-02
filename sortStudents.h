2ae70cac337b0340f42d4be56164f60506895e6d
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2024-01-02 18:08:05 +0200

    Update sortStudents.h

--- sortStudents.h
+++ sortStudents.h
@@ -8,6 +8,6 @@ bool compareByName(const Studentas& a, const Studentas& b);
 bool compareBySurname(const Studentas& a, const Studentas& b);
 bool compareByAverage(const Studentas& a, const Studentas& b);
 bool compareByMedian(const Studentas& a, const Studentas& b);
-void sortStudents(std::vector<Studentas>& studentai);
+void sortStudents(std::vector<Studentas>& studentai, int sortOption);
 
 #endif
