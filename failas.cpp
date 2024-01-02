4e56556d369c5380251734cee70dbad218254099
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2024-01-02 15:04:29 +0200

    Update failas.cpp

--- failas.cpp
+++ failas.cpp
@@ -67,63 +67,80 @@ void isFailo(std::vector<Studentas>& studentai, int a, const string& filename) {
 
     failas.close();
 
+    vector<Studentas> vargsiukai;
+    vector<Studentas> kietiakai;
+
+    int Strat;
+    std::cout << "Kokia Strategija naudoti?: 1 - Strategija1, 2 - Strategija2: ";
+    std::cin >> Strat;
+
     int sortOption;
     std::cout << "Kaip norite rusiuoti studentus?: 1 - Pagal varda, 2 - Pagal pavarde, 3 - Pagal vidurki, 4 - Pagal mediana: ";
     std::cin >> sortOption;
 
-    // Create two new vectors
-    vector<Studentas> vargsiukai;
-//    vector<Studentas> kietiakai;
-
-    // Split the students into two vectors
-/*
-    for (const Studentas& studentas : studentai) {
-        if (a == 1) {
-            if (studentas.vidurkis >= 5) {
-                kietiakai.push_back(studentas);
-            } else {
-                vargsiukai.push_back(studentas);
-            }
-        } else if (a == 2) {
-            if (studentas.mediana >= 5) {
-                kietiakai.push_back(studentas);
-            } else {
-                vargsiukai.push_back(studentas);
+    if (Strat==1){
+        for (const Studentas& studentas : studentai) {
+            if (a == 1) {
+                if (studentas.vidurkis >= 5) {
+                    kietiakai.push_back(studentas);
+                } else {
+                    vargsiukai.push_back(studentas);
+                }
+            } else if (a == 2) {
+                if (studentas.mediana >= 5) {
+                    kietiakai.push_back(studentas);
+                } else {
+                    vargsiukai.push_back(studentas);
+                }
             }
         }
+        start = high_resolution_clock::now();
+
+        sortStudents(kietiakai, sortOption);
+        sortStudents(vargsiukai, sortOption);
+
+        stop = high_resolution_clock::now();
+        duration = duration_cast<microseconds>(stop - start);
+
+        std::cout << "Rusiavimas";
+        std::cout << std::endl;
+        std::cout << "Trukme: "
+            << duration.count() << " microseconds" << std::endl;
+    } else {
+        studentai.erase(std::remove_if(studentai.begin(), studentai.end(), [&](const Studentas& studentas) {
+            bool isVargsiukas = (a == 1 && studentas.vidurkis < 5) || (a == 2 && studentas.mediana < 5);
+            if (isVargsiukas) {
+                vargsiukai.push_back(studentas);
+            }
+            return isVargsiukas;
+        }), studentai.end());
+        start = high_resolution_clock::now();
+
+        sortStudents(studentai, sortOption);
+        sortStudents(vargsiukai, sortOption);
+
+        stop = high_resolution_clock::now();
+        duration = duration_cast<microseconds>(stop - start);
+
+        std::cout << "Rusiavimas";
+        std::cout << std::endl;
+        std::cout << "Trukme: "
+            << duration.count() << " microseconds" << std::endl;
+
     }
-*/
-    studentai.erase(std::remove_if(studentai.begin(), studentai.end(), [&](const Studentas& studentas) {
-        bool isVargsiukas = (a == 1 && studentas.vidurkis < 5) || (a == 2 && studentas.mediana < 5);
-        if (isVargsiukas) {
-            vargsiukai.push_back(studentas);
-        }
-        return isVargsiukas;
-    }), studentai.end());
-
-    start = high_resolution_clock::now();
-
-    // Sort the students
-    //sortStudents(kietiakai, sortOption);
-    sortStudents(studentai, sortOption);
-    sortStudents(vargsiukai, sortOption);
-
-    stop = high_resolution_clock::now();
-    duration = duration_cast<microseconds>(stop - start);
-
-    std::cout << "Rusiavimas";
-    std::cout << std::endl;
-    std::cout << "Trukme: "
-         << duration.count() << " microseconds" << std::endl;
-
 
     // Start timing
     start = high_resolution_clock::now();
 
+    int size = (Strat == 1) ? studentai.size() : (studentai.size() + vargsiukai.size());
+
     // Write the students to files
-    ofstream failas1("kietiakai" + to_string(studentai.size()) + ".txt");
-    ofstream failas2("vargsiukai" + to_string(studentai.size()) + ".txt");
-    for (const Studentas& studentas : /*kietiakai*/studentai) {
+    ofstream failas1("kietiakai" + to_string(size) + ".txt");
+    ofstream failas2("vargsiukai" + to_string(size) + ".txt");
+
+    vector<Studentas>& target = (Strat == 1) ? kietiakai : studentai;
+
+    for (const Studentas& studentas : target) {
         failas1 << studentas.vardas << "\t" << studentas.pavarde << "\t";
         if (a == 1) {
             failas1 << studentas.vidurkis << endl;
