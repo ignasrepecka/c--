9d99577a53b29c1d8bfe6673ed9261e3bdcb82d2
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2024-01-02 18:00:29 +0200

    Update listSortStudents.cpp

--- listSortStudents.cpp
+++ listSortStudents.cpp
@@ -20,19 +20,7 @@ bool compareByMedian(const Studentasl& a, const Studentasl& b) {
     return a.mediana < b.mediana;
 }
 
-void listSortStudents(std::list<Studentasl>& studentail) {
-    int sortOption;
-    std::cout << "Kaip norite rusiuoti studentus?: 1 - Pagal varda, 2 - Pagal pavarde, 3 - Pagal vidurki, 4 - Pagal mediana: ";
-    std::cin >> sortOption;
-
-    // Declare timing variables
-    std::chrono::high_resolution_clock::time_point start;
-    std::chrono::high_resolution_clock::time_point stop;
-    std::chrono::duration<double, std::micro> duration;
-
-    // Start timing
-    start = std::chrono::high_resolution_clock::now();
-
+void listSortStudents(std::list<Studentasl>& studentail, int sortOption) {
     switch (sortOption) {
         case 1:
             studentail.sort(compareByName);  // To sort by name
@@ -50,12 +38,4 @@ void listSortStudents(std::list<Studentasl>& studentail) {
             std::cout << "Netinkamas rūšiavimo pasirinkimas. Prašome pasirinkti nuo 1 iki 4." << std::endl;
             break;
     }
-
-    // Stop timing and calculate the duration
-    stop = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    std::cout << "Rusiavimas";
-    std::cout << std::endl;
-    std::cout << "Trukme: "
-         << duration.count() << " microseconds" << std::endl;
 }
