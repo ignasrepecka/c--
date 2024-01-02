5b01732e011adc9f623f40516873d5538be37839
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2024-01-03 00:19:17 +0200

    Update strat.cpp

--- strat.cpp
+++ strat.cpp
@@ -14,6 +14,7 @@ using namespace std::chrono;
 
 void strat(std::vector<Studentas>& studentai, int a, int Strat, int sortOption, std::vector<Studentas>& kietiakai, std::vector<Studentas>& vargsiukai) {
         if (Strat==1){
+        auto start = high_resolution_clock::now();
         for (const Studentas& studentas : studentai) {
             if (a == 1) {
                 if (studentas.vidurkis >= 5) {
@@ -29,7 +30,6 @@ void strat(std::vector<Studentas>& studentai, int a, int Strat, int sortOption, 
                 }
             }
         }
-        auto start = high_resolution_clock::now();
 
         sortStudents(kietiakai, sortOption);
         sortStudents(vargsiukai, sortOption);
@@ -41,17 +41,42 @@ void strat(std::vector<Studentas>& studentai, int a, int Strat, int sortOption, 
         std::cout << std::endl;
         std::cout << "Trukme: "
             << duration.count() << " microseconds" << std::endl;
-    } else {
+        } else if (Strat==2) {
+        auto start = high_resolution_clock::now();
         studentai.erase(std::remove_if(studentai.begin(), studentai.end(), [&](const Studentas& studentas) {
             bool isVargsiukas = (a == 1 && studentas.vidurkis < 5) || (a == 2 && studentas.mediana < 5);
             if (isVargsiukas) {
                 vargsiukai.push_back(studentas);
             }
             return isVargsiukas;
-        }), studentai.end());
-        auto start = high_resolution_clock::now();
+            }), studentai.end());
 
-        sortStudents(studentai, sortOption);
+            sortStudents(studentai, sortOption);
+            sortStudents(vargsiukai, sortOption);
+
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+
+            std::cout << "Rusiavimas";
+            std::cout << std::endl;
+            std::cout << "Trukme: "
+                << duration.count() << " microseconds" << std::endl;
+
+        } else {
+        auto start = high_resolution_clock::now();
+            auto it = std::partition(studentai.begin(), studentai.end(), [a](const Studentas& studentas) {
+                if (a == 1) {
+                    return studentas.vidurkis >= 5;
+                } else if (a == 2) {
+                    return studentas.mediana >= 5;
+                }
+                return false;
+            });
+
+        kietiakai.assign(studentai.begin(), it);
+        vargsiukai.assign(it, studentai.end());
+
+        sortStudents(kietiakai, sortOption);
         sortStudents(vargsiukai, sortOption);
 
         auto stop = high_resolution_clock::now();
@@ -61,6 +86,5 @@ void strat(std::vector<Studentas>& studentai, int a, int Strat, int sortOption, 
         std::cout << std::endl;
         std::cout << "Trukme: "
             << duration.count() << " microseconds" << std::endl;
-
-    }
+        }
 }
