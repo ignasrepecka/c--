ea31aad47eebe977a9d40171506805d228288102
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2024-01-02 18:06:05 +0200

    Update studentai.h

--- studentai.h
+++ studentai.h
@@ -1,12 +1,13 @@
 #ifndef STUDENTAI_H
 #define STUDENTAI_H
 
+#include <list>
 #include <vector>
 #include <string>
 #include "calculateStatistics.h"
+#include "listCalculateStatistics.h"
 
-void ivestiStudentus(int n, int a);
-void generuoti(int n, int a);
 void isFailo(std::vector<Studentas>& studentai, int a);
+void listIsFailo(std::list<Studentasl>& studentail, int a);
 
 #endif // STUDENTAI_H
